1ccef87fb90e74a27ad794fa82e8fa642b68fa4a
yangyang0508 <yangyang0508@gmail.com>
2023-01-06 23:39:42 -0800

    user register

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.RegisterRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @PostMapping("/signup")
+    public void signup(@RequestBody RegisterRequest regierRequest){
+
+    }
+}
---
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -0,0 +1,47 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.RegisterRequest;
+import com.example.demo.model.User;
+import com.example.demo.model.VerificationToken;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+
+public class AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationTokenRepository.save(verificationToken);
+        return token;
+    }
+}

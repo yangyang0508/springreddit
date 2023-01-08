4b1c5d1f788db27f3007c1eb4eb503b57ed306bc
yangyang0508 <yangyang0508@gmail.com>
2023-01-07 23:14:24 -0800

    user registeration 2

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,6 +1,10 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.RegisterRequest;
+import com.example.demo.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AuthController {
-    @PostMapping("/signup")
-    public void signup(@RequestBody RegisterRequest regierRequest){
 
+    private final AuthService authService;
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest regierRequest){
+        authService.signup(regierRequest);
+        return new ResponseEntity<>("User Registeration Successfully", HttpStatus.OK);
     }
 }
--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.RegisterRequest;
+import com.example.demo.model.NotificationEmail;
 import com.example.demo.model.User;
 import com.example.demo.model.VerificationToken;
 import com.example.demo.repository.UserRepository;
@@ -22,6 +23,7 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final VerificationTokenRepository verificationTokenRepository;
+    private final MailService mailService;
 
     @Transactional
     public void signup(RegisterRequest registerRequest) {
@@ -34,7 +36,10 @@ public class AuthService {
 
         userRepository.save(user);
 
-        String token = generateVerificationToken(user);
+        //String token = generateVerificationToken(user);
+       // mailService.sendMail(new NotificationEmail("Please activate your account", user.getEmail(), "Thank you for signing up to Spring Reddit, " +
+       //         "please click on the below url to activate your account : " +
+         //       "http://localhost:8080/api/auth/accountVerification/" + token));
     }
 
     private String generateVerificationToken(User user) {

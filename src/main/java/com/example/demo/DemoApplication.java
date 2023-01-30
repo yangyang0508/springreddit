18f6907346f6ca6522ce7bdad31edea6ad8dc970
yangyang0508 <yangyang0508@gmail.com>
2023-01-29 22:41:36 -0800

    swagger

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,16 @@
 package com.example.demo;
 
+import com.example.demo.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
